d141b1a12a6460763c2d21fd3840afb24644d451
vocka <vockalimo@gmail.com>
2022-02-22 17:59:05 +0800

    init

---
+++ resources/js/components/Imagesearchupload.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Image upload</div>
+                <div class="card-content">
+                    <form>
+                        <input type="file" class="form-control" @onchange="onChange">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+name: "Imagesearch",
+    methods: {
+    onChange(e) {
+        this.file = e.target.files[0];
+    }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/Welcome.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>{{ title }}</h1>
+</template>
+
+<script>
+export default {
+    setup: () => ({
+        title: 'How To Install Vue 3 in Laravel 8 From Scratch'
+    }),
+    name: "Welcome.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
