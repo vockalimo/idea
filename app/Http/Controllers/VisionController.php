2164a717e2879646eda088c1ca43e0814ac80a62
vocka <vockalimo@gmail.com>
2022-03-25 16:10:03 +0800

    add auth

--- app/Http/Controllers/VisionController.php
+++ app/Http/Controllers/VisionController.php
@@ -28,6 +28,7 @@ class VisionController extends Controller
         header("Access-Control-Allow-Origin: *");
         header("Access-Control-Allow-Methods: *");
         header("Access-Control-Allow-Headers: Origin, Methods, Content-Type");
+        header("Access-Control-Allow-Headers: Authorization");
 
         try {
             $validateResult = $this->validate($request, [
